cc79e6b67c9b6148eee7ac8b54dfa3da92b41bba
felix mwangi <fmwauramwangi@gmail.com>
2025-01-18 13:36:21 +0300

    src/components/EyesOnMe.js

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
-// Code Keypad Component Here
+import React from "react";
+
 
 function Keypad (){
+    const handleChange = () => {
+        console.log('Entering password...');
+    };
     return (
-        <div></div>
-    )
+        <input
+            type="password"
+            onChange={handleChange}
+            />
+
+        );
 }
 
 export default Keypad;
